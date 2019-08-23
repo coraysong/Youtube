0075a6a1fea563310e97828e0c876632e3a34065
coraysong <coraysong1991@msn.cn>
2019-08-23 21:59:02 +0800

    选项菜单完成

--- Youtube/Youtube/Controller/SettingsLauncher.swift
+++ Youtube/Youtube/Controller/SettingsLauncher.swift
@@ -8,20 +8,39 @@
 
 import UIKit
 
-class SettingsLauncher: NSObject {
+class Setting: NSObject {
+    let name: String
+    let imageName: String
     
-    let height:CGFloat = 200
+    init(name: String, imageName: String) {
+        self.name = name
+        self.imageName = imageName
+    }
+}
+
+class SettingsLauncher: NSObject, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "cellId"
+    let cellHeight: CGFloat = 50
+    
+    
+    let settings: [Setting] = {
+        return [
+            Setting(name: "Settings", imageName: "settings"),
+            Setting(name: "Terms & privacy policy", imageName: "privacy"),
+            Setting(name: "Send Feedback", imageName: "feedback"),
+            Setting(name: "Help", imageName: "help"),
+            Setting(name: "Switch Account", imageName: "switch_account"),
+            Setting(name: "Cancel", imageName: "cancel")
+        ]
+    }()
+    
+    
+    
     //    let y:CGFloat = UIScreen.main.bounds.height - height!
     
-    let y: CGFloat = UIScreen.main.bounds.height - 200
-    override init() {
-        super.init()
-        
-    }
     
     let blackView = UIView()
-    
-
     
     let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -33,6 +52,8 @@ class SettingsLauncher: NSObject {
     
     func showSettings() {
         //显示设置选项
+        let height:CGFloat = CGFloat(settings.count) * cellHeight
+        let y: CGFloat = UIScreen.main.bounds.height - height
         if let window = UIApplication.shared.keyWindow {
             blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
             blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDissmiss)))
@@ -47,7 +68,7 @@ class SettingsLauncher: NSObject {
             
             UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
                 self.blackView.alpha = 1
-                self.collectionView.frame = CGRect(x: 0, y: self.y, width: window.frame.width, height: self.height)
+                self.collectionView.frame = CGRect(x: 0, y: y, width: window.frame.width, height: height)
             })
         }
     }
@@ -56,11 +77,38 @@ class SettingsLauncher: NSObject {
         
         UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
             self.blackView.alpha = 0
-            self.collectionView.frame = CGRect(x: 0, y: UIScreen.main.bounds.height, width: UIScreen.main.bounds.width, height: self.height)
+            if let window = UIApplication.shared.keyWindow {
+                self.collectionView.frame = CGRect(x: 0, y: window.frame.height, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+            }
         })
     }
     
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return settings.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SettingCell
+        cell.setting = settings[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.size.width, height: cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    override init() {
+        super.init()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(SettingCell.self, forCellWithReuseIdentifier: cellId)
+    }
     
 }
 
